b1c7f088f268e3b77da3bb20d676fca7a5411a14
danieljml <danielgarrido425@gmail.com>
2023-12-04 04:06:36 -0400

    Putting pathname outside of the function

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -8,12 +8,12 @@ import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (currentPage: number | string) => {
-    const pathname = usePathname();
     const params = new URLSearchParams(searchParams)
     params.set('page', currentPage.toString())
     return `${pathname}?${params.toString()}`
